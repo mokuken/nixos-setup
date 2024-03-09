37ce48a7758d966bf87b642e6ef25e5430d921bc
mokuken <imsilencer44@gmail.com>
2024-03-09 18:21:10 +0800

    added nixos config and gtk theme

---
+++ nixos/configuration.nix
@@ -0,0 +1,170 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mokuken = {
+    isNormalUser = true;
+    description = "MokuKen";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   # Status bar
+   (waybar.overrideAttrs (oldAttrs: {
+     mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    })
+   )
+
+   # Notifications
+   libnotify
+   dunst
+
+   # Wallpaper Daemon
+   swww
+
+   # Terminal emulator
+   kitty
+
+   # Launcher
+   rofi-wayland
+
+   # Dev tools
+   git
+
+   # File manager
+   gnome.nautilus
+
+   # Text / Code editor
+   micro
+   vscodium
+
+   # Browser
+   ungoogled-chromium
+
+   # Other pkgs
+   neofetch
+  ];
+
+  # enable qt and theme
+ # qt = {
+  #  enable = true;
+   # platformTheme = "gtk2";
+    #style = "adwaita-dark";
+  #};
+
+  # Remove default programs
+  programs.nano.enable = false;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.xserver.desktopManager.xterm.enable = false;
+
+  # Set new default text editor
+  environment.variables = {
+   EDITOR = "macro";
+  };
+
+  # Setting fonts
+  fonts.packages = with pkgs; [
+   material-design-icons
+   font-awesome
+  ];
+
+  # Enable display maanagers
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  # Enable hyprland
+  programs.hyprland.enable = true;
+
+  # Desktop portals
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+   enable = true;
+   alsa.enable = true;
+   alsa.support32Bit = true;
+   pulse.enable = true;
+   jack.enable = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
