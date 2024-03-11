91f07d701809ce20e3e5cb20890f80fd6b544407
mokuken <imsilencer44@gmail.com>
2024-03-11 10:32:54 +0800

    fixed start script + randomizer wallpaper

--- config/hypr/start.sh
+++ config/hypr/start.sh
@@ -1,14 +1,47 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
 
 # Running waybar
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
+if pgrep waybar > /dev/null
+then
+    echo "waybar is already running"
+else
+    waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
+fi
 
 # Setting wallpaper
-swww img /home/mokuken/Downloads/Espresso.jpg
+IMAGE_DIR="/home/mokuken/.config/hypr/waves"
+
+if [ ! -d "$IMAGE_DIR" ]; then
+  echo "Error: Image directory not found."
+  exit 1
+fi
+
+images=("$IMAGE_DIR"/*)
+
+if [ ${#images[@]} -eq 0 ]; then
+  echo "Error: No image files found in the directory."
+  exit 1
+fi
+
+num_images=${#images[@]}
+
+random_index=$(( RANDOM % num_images ))
+
+random_image="${images[$random_index]}"
+
+echo "Randomly selected image: $random_image"
+
+swww img "$random_image"
+
 
 # Initialize wallpaper daemon
-swww init &
+if pgrep swww > /dev/null
+then
+    echo "swww is already running"
+else
+    swww init &
+fi
 
 # Running dunst
 # dunst
